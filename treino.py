088d927acbe873cb6ee3ec2350d3251b76790d63
Gustavo <gustavosbortolon.com>
2026-03-10 23:54:18 -0300

    Aplicacao do label e one hot encoder

--- treino.py
+++ treino.py
@@ -2,13 +2,15 @@ import pandas as ps
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from category_encoders.one_hot import OneHotEncoder
 
 base = ps.read_csv('titanic_train.csv')
 
 ## preenchendo valores vazios com a mediana da idade
 
 # Extrai o título que vem antes do ponto final no nome
-base['Title'] = base['Name'].str.extract(' ([A-Za-z]+)\.', expand=False)
+base['Title'] = base['Name'].str.extract(r' ([A-Za-z]+)\.', expand=False)
 
 # Agrupa por título e calcula a mediana da idade
 medians_by_title = base.groupby('Title')['Age'].median()
@@ -35,11 +37,26 @@ print(f"Existem {len(outliers)} passageiros que pagaram valores muito acima da m
 
 ## separando coluna cabin
 
-print(base.info)
-print(base)
+
 #criei uma nova coluna 'Deck' e utilizei uma regex para selecionar só o caracter não numérico da coluna 'Cabin'
 base['Deck']  = base['Cabin'].replace('[^A-Z]','',regex=True).astype('string')
 
+#Todos os NaN são substituidos por 'U' de Unknown
 base['Deck'] = base['Deck'].fillna('U')
+
+
+
+## aplicando label encoder e one hot enconder
+
+#aplicando label encoder na coluna 'Sex'
+label_encoder = LabelEncoder()
+base['Sex'] = label_encoder.fit_transform(base['Sex'])
+
 print(base)
+
+# Criando colunas extras para Embarked e Deck (One-Hot Encoding)
+base = ps.get_dummies(base, columns=['Embarked', 'Deck'])
+
+# Removendo o que sobrou de "lixo" (colunas que não são números)
+base.drop(['Name', 'Ticket', 'Cabin', 'PassengerId'], axis=1, inplace=True, errors='ignore')
 
